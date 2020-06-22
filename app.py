b5eff089c181002ffae482f071d7baed3bd9aa3e
Miha-Mak <66954500+Miha-MAK@users.noreply.github.com>
2020-06-23 02:54:27 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,91 @@
+import json
+from datetime import datetime
+import telebot
+from telebot import types
+from telebot.apihelper import _make_request
+import pytz
+from time import sleep
+
+bot = telebot.TeleBot("1206195696:AAGMrhQEMMnzyjP5VukNUBS2WgwlE8-jFBw")
+chat_for = "-1001477648063"
+
+my_permissions = {
+    "can_send_messages": False,
+    "can_send_media_messages": False,
+    "can_send_polls": False,
+    "can_send_other_messages": False
+}
+
+allowed_permissions = {
+    "can_send_messages": True,
+    "can_send_media_messages": True,
+    "can_send_polls": True,
+    "can_send_other_messages": True
+}
+
+new_permissions = types.ChatPermissions(**my_permissions)
+allowed_permissions = types.ChatPermissions(**allowed_permissions)
+
+print("start")
+print(f"{bot.get_me().username}")
+
+with open("interval.txt", "r") as f:
+    interval = f.read()
+
+
+
+@bot.message_handler(commands=['start'])
+def any_msg(message):
+
+    keyboard = types.InlineKeyboardMarkup(row_width=1)
+    callback_button_yes = types.InlineKeyboardButton(text="Поставить время⏳", callback_data=f"yes{message.from_user.id}")
+    callback_button_no = types.InlineKeyboardButton(text="Хочу такого же бота🤖", callback_data="no")
+    keyboard.add(callback_button_yes, callback_button_no)
+    bot.send_message(message.chat.id, """Добро пожаловать {}!🙋‍♂️
+Выберите пожалуйста причину вашего входа👇🏻""".format(message.from_user.first_name),reply_markup=keyboard)
+
+
+@bot.message_handler()
+def do_freeze(message):
+    check = "undo"
+    now = "ye"
+    while check == "undo":
+        sleep(20)
+        if datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H%M") == message.text:
+            bot.set_chat_permissions(chat_for, new_permissions)
+            now = int(datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H%M"))
+            bot.send_message(message.chat.id, """Установлен час тишины✅
+Для всех участников чата активирована режим чтения⏳""")
+            bot.send_message(chat_for, """Установлен час тишины✅
+Для всех участников чата активирована режим чтения⏳""")
+            check = "do"
+            print(now)
+
+    while check == "do":
+        if int(datetime.now(pytz.timezone("Europe/Moscow")).strftime("%H%M")) == int(now) + int(interval):
+            bot.set_chat_permissions(chat_for, allowed_permissions)
+            bot.send_message(message.chat.id, """Ограничения сняты✅
+Участники могут отправлять сообщения!🥳""")
+            bot.send_message(chat_for, """Ограничения сняты✅
+Участники могут отправлять сообщения!🥳""")
+            print("uspex1")
+            check = "ando"
+
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data.startswith("yes"):
+        global ID
+        ID = str(call.data.replace("yes","")) # ID клиента
+
+        s = bot.send_message(call.message.chat.id, text = "Напишите начальное время в нужном формате...📝")
+
+        bot.register_next_step_handler(s,do_freeze) # Переходим в do_freeze
+
+
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, text = "Позже здесь появится инструкция для поставки бота на вашу группу)")
+
+
+bot.polling()
